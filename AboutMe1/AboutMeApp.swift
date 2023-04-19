aa3a7a7232c9d464cf0c72cf678f72920ca6a93f
Aficer <Aficer89@gmail.com>
2023-04-19 15:14:09 +0300

    AbotMe
    test

---
+++ AboutMe1/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Вадим Эфис on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe1/Data.swift
@@ -0,0 +1,38 @@
+//
+//  Data.swift
+//  AboutMe
+//
+//  Created by Вадим Эфис on 16.04.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Info {
+    let image: String
+    let name: String
+    let story: String
+    let hobbies: [String]
+    let foods: [String]
+    let colors: [Color]
+    let funFacts: [String]
+}
+
+let information = Info(
+    image: "Placeholder",
+    name: "Вадим",
+    story: "A story can be about anything you can dream up. There are no right answers, there is no one else.\n\nNeed some inspiration?(Придумать, написать историю)\n• 🐶🐱🛶️🎭🎤🎧🎸\n• 🏄‍♀️🚵‍♀️🚴‍♀️⛵️🥾🏂⛷📚\n• ✍️🥖☕️🏋️‍♂️🚲🧗‍♀️ ",
+    hobbies: ["bicycle", "film.fill", "book.fill"],
+    foods: ["🥐", "🌮", "🍣"],
+    colors: [Color.red, Color.purple, Color.black],
+    funFacts: [
+        "The femur is the longest and largest bone in the human body.",
+        "The moon is 238,900 miles away.",
+        "Prince’s last name was Nelson.",
+        "503 new species were discovered in 2020.",
+        "Ice is 9 percent less dense than liquid water.",
+        "You can spell every number up to 1,000 without using the letter A.\n\n...one, two, three, four...ninety-nine...nine hundred ninety-nine 🧐",
+        "A collection of hippos is called a bloat.",
+        "White sand beaches are made of parrotfish poop.",
+    ]
+)
